9f2b451360146c198ef150505bc6ccf0fd162b22
Ali Hosseini <ali762124@gmail.com>
2022-03-14 03:12:55 -0700

    The template was added to the main project and modifications were made

---
+++ public/front/js/custom.js
@@ -0,0 +1,24 @@
+$('.counter-number').counterUp({
+    delay: 10,
+    time: 3000
+});
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar-nav');
+const closeMenu = document.querySelector('.close-menu');
+const overlayMenu = document.querySelector('.header-overlay-menu');
+
+hamburgerMenu.addEventListener('click', function () {
+    navbar.classList.add('show');
+    overlayMenu.classList.add('display');
+});
+
+closeMenu.addEventListener('click', function () {
+    navbar.classList.remove('show');
+    overlayMenu.classList.remove('display');
+});
+
+overlayMenu.addEventListener('click', function () {
+    navbar.classList.remove('show');
+    overlayMenu.classList.remove('display');
+});
