503c5ac3f97bdb3ac9b266148e03a7c3cad77ed2
Ali Hosseini <ali762124@gmail.com>
2022-03-15 02:44:46 -0700

    The Laravel Breeze package was added to the project

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
     return view('front.index');
-});
+})->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
