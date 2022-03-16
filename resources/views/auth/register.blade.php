30c1ec5d49a9e00afb09f7c20bc6748c047b423f
Ali Hosseini <ali762124@gmail.com>
2022-03-15 23:20:20 -0700

    The user registration operation was completed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,13 +7,20 @@
         <div class="container py-5 my-5">
             <div class="row justify-content-center">
                 <div class="col-md-6">
+
+                    @foreach ($errors->all() as $error)
+                        <p>{{ $error }}</p>
+                    @endforeach
+
                     <h3 class="text-center text-dark fw-bold mb-5">ثبت نام</h3>
                     <form action="{{ route('register.store') }}" method="post">
-                        <input type="text" placeholder="نام و نام خانوادگی شما" class="form-control mt-3 p-2">
-                        <input type="number" placeholder="شماره موبایل" class="form-control mt-3 p-2">
-                        <input type="email" placeholder="آدرس ایمیل" class="form-control mt-3 p-2">
-                        <input type="text" placeholder="رمز عبور" class="form-control mt-3 p-2">
-                        <input type="text" placeholder="تکرار رمز عبور" class="form-control mt-3 p-2">
+                        @csrf
+                        <input type="text" name="name" placeholder="نام و نام خانوادگی شما" class="form-control mt-3 p-2">
+                        <input type="number" name="mobile" placeholder="شماره موبایل" class="form-control mt-3 p-2">
+                        <input type="email" name="email" placeholder="آدرس ایمیل" class="form-control mt-3 p-2">
+                        <input type="password" name="password" placeholder="رمز عبور" class="form-control mt-3 p-2">
+                        <input type="password" name="password_confirmation" placeholder="تکرار رمز عبور"
+                            class="form-control mt-3 p-2">
                         <button type="submit" class="btn btn-primary text-dark w-100 mt-3 border-0">ثبت نام</button>
                         <a href="" class="btn btn-success text-white w-100 mt-3 border-0">صفحه ورود به حساب کاربری</a>
                     </form>
