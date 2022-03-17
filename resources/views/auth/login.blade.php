a5ade494bd0fa2fb32c75db77a2975cec7be7922
Ali Hosseini <ali762124@gmail.com>
2022-03-17 06:22:50 -0700

    Users can enter the site via email and mobile number

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,8 +15,12 @@
                     <h3 class="text-center text-dark fw-bold mb-5">ورود به حساب کاربری</h3>
                     <form action="{{ route('login.store') }}" method="post">
                         @csrf
-                        <input type="email" name="email" placeholder="آدرس ایمیل" class="form-control mt-3 p-2">
+                        <input type="text" name="email" placeholder="ایمیل یا موبایل" class="form-control mt-3 p-2">
                         <input type="password" name="password" placeholder="رمز عبور" class="form-control mt-3 p-2">
+                        <label for="remember" class="mt-3">
+                            <input type="checkbox" name="remember" id="remember">
+                            <span>مرا بخاطر بسپار</span>
+                        </label>
                         <button type="submit" class="btn btn-primary text-dark w-100 mt-3 border-0">ورود</button>
                         <a href="{{ route('register') }}" class="btn btn-success text-white w-100 mt-3 border-0">صفحه ثبت
                             نام</a>
