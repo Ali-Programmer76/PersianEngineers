95cf8a74ede77d23c3f715b04b968cb469ae8f67
Ali Hosseini <ali762124@gmail.com>
2022-03-18 01:31:39 -0700

    Registration and login errors were translated into Persian

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,19 +7,20 @@
         <div class="container py-5 my-5">
             <div class="row justify-content-center">
                 <div class="col-md-6">
-
-                    @foreach ($errors->all() as $error)
-                        <p>{{ $error }}</p>
-                    @endforeach
-
                     <h3 class="text-center text-dark fw-bold mb-5">ورود به حساب کاربری</h3>
                     <form action="{{ route('login.store') }}" method="post">
                         @csrf
                         <input type="text" name="email" placeholder="ایمیل یا موبایل" class="form-control mt-3 p-2">
+                        @error('email')
+                            <p class="text-danger my-2">{{ $message }}</p>
+                        @enderror
                         <input type="password" name="password" placeholder="رمز عبور" class="form-control mt-3 p-2">
+                        @error('password')
+                            <p class="text-danger my-2">{{ $message }}</p>
+                        @enderror
                         <label for="remember" class="mt-3">
                             <input type="checkbox" name="remember" id="remember">
-                            <span>مرا بخاطر بسپار</span>
+                            <span class="remember">مرا بخاطر بسپار</span>
                         </label>
                         <button type="submit" class="btn btn-primary text-dark w-100 mt-3 border-0">ورود</button>
                         <a href="{{ route('register') }}" class="btn btn-success text-white w-100 mt-3 border-0">صفحه ثبت
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,18 +7,25 @@
         <div class="container py-5 my-5">
             <div class="row justify-content-center">
                 <div class="col-md-6">
-
-                    @foreach ($errors->all() as $error)
-                        <p>{{ $error }}</p>
-                    @endforeach
-
                     <h3 class="text-center text-dark fw-bold mb-5">ثبت نام</h3>
                     <form action="{{ route('register.store') }}" method="post">
                         @csrf
                         <input type="text" name="name" placeholder="نام و نام خانوادگی شما" class="form-control mt-3 p-2">
+                        @error('name')
+                            <p class="text-danger my-2">{{ $message }}</p>
+                        @enderror
                         <input type="number" name="mobile" placeholder="شماره موبایل" class="form-control mt-3 p-2">
+                        @error('mobile')
+                            <p class="text-danger my-2">{{ $message }}</p>
+                        @enderror
                         <input type="email" name="email" placeholder="آدرس ایمیل" class="form-control mt-3 p-2">
+                        @error('email')
+                            <p class="text-danger my-2">{{ $message }}</p>
+                        @enderror
                         <input type="password" name="password" placeholder="رمز عبور" class="form-control mt-3 p-2">
+                        @error('password')
+                            <p class="text-danger my-2">{{ $message }}</p>
+                        @enderror
                         <input type="password" name="password_confirmation" placeholder="تکرار رمز عبور"
                             class="form-control mt-3 p-2">
                         <button type="submit" class="btn btn-primary text-dark w-100 mt-3 border-0">ثبت نام</button>
