9ee618c4ec3f0f391b67117d31ce28b801223888
Ali Hosseini <ali762124@gmail.com>
2022-03-24 02:07:50 -0700

    The access level of the site users was determined

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -11,60 +11,64 @@
         <span class="sidebar-icon"><i class="fas fa-home"></i></span>
         داشبورد
     </a>
-    <a href="" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-ellipsis-h"></i></span>
-        منوی بالایی
-    </a>
-    <a href="" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-laptop-house"></i></span>
-        خانه
-    </a>
-    <a href="" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-address-card"></i></span>
-        درباره ما
-    </a>
-    <a href="" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-book-open"></i></span>
-        مقدمه
-    </a>
-    <a href="" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-cogs"></i></span>
-        خدمات
-    </a>
-    <a href="" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-user-friends"></i></span>
-        تیم ها
-    </a>
-    <a href="" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-images"></i></span>
-        نمونه کارها
-    </a>
-    <a href="" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-comment-alt"></i></span>
-        نظرات مشتریان
-    </a>
-    <a href="" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-headset"></i></span>
-        سؤالات متداول
-    </a>
-    <a href="" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-clipboard-list"></i></span>
-        فوتر سایت
-    </a>
-    <a href="{{ route('users.index') }}" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-users"></i></span>
-        کاربران سایت
-    </a>
-    <a href="" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-comment"></i></span>
-        نظرات
-    </a>
-    <a href="" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-users"></i></span>
-        پیام کاربران
-    </a>
-    <a href="" class="sidebar-link">
-        <span class="sidebar-icon"><i class="fas fa-blog"></i></span>
-        بلاگ ها
-    </a>
+    @if (auth()->user()->role === 'admin')
+        <a href="" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-ellipsis-h"></i></span>
+            منوی بالایی
+        </a>
+        <a href="" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-laptop-house"></i></span>
+            خانه
+        </a>
+        <a href="" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-address-card"></i></span>
+            درباره ما
+        </a>
+        <a href="" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-book-open"></i></span>
+            مقدمه
+        </a>
+        <a href="" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-cogs"></i></span>
+            خدمات
+        </a>
+        <a href="" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-user-friends"></i></span>
+            تیم ها
+        </a>
+        <a href="" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-images"></i></span>
+            نمونه کارها
+        </a>
+        <a href="" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-comment-alt"></i></span>
+            نظرات مشتریان
+        </a>
+        <a href="" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-headset"></i></span>
+            سؤالات متداول
+        </a>
+        <a href="" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-clipboard-list"></i></span>
+            فوتر سایت
+        </a>
+        <a href="{{ route('users.index') }}" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-users"></i></span>
+            کاربران سایت
+        </a>
+    @endif
+    @if (auth()->user()->role === 'admin' || auth()->user()->role === 'author')
+        <a href="" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-comment"></i></span>
+            نظرات
+        </a>
+        <a href="" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-users"></i></span>
+            پیام کاربران
+        </a>
+        <a href="" class="sidebar-link">
+            <span class="sidebar-icon"><i class="fas fa-blog"></i></span>
+            بلاگ ها
+        </a>
+    @endif
 </div>
